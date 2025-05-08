6ecc871b48ff76f305b113e374d1e0a170a3b345
Stephen Amponsem <stephen013amponsem@gmail.com>
2025-05-08 16:16:29 +0000

    added binary search on continuous fn soln

--- DSA/binary_search.py
+++ DSA/binary_search.py
@@ -227,3 +227,68 @@ end = 5  # ending point of the interval
 
 result = binary_search(f, target, start, end, epsilon)
 print("The value of x for which f(x) is approximately 0 within the interval [" + str(start) + ", " + str(end) + "] is: ", result)
+
+
+
+
+
+
+# Python program to find the root of a given function using Binary Search
+import math
+import numpy as np
+
+# Define a continuous function 'f' where f(x) = x^4 - x^2 - 10
+def f(x):
+    return x**4 - x**2 - 10
+
+# Define the binary search function 
+def binary_search(func, target, left, right, precision):
+    while np.abs(func(left)) > precision and np.abs(func(right)) > precision:
+        middle = (left + right) / 2
+        if func(middle) < target:
+            left = middle
+        else:
+            right = middle
+            
+    return middle
+
+epsilon = 1e-6  # to make sure the solution is within an acceptable range
+target = 50  # target value for root of function 'f'
+start = -5  # starting point of the interval
+end = 5  # ending point of the interval
+
+result = binary_search(f, target, start, end, epsilon)
+print("The value of x for which f(x) is approximately 0 within the interval [" + str(start) + ", " + str(end) + "] is: ", result)
+
+
+
+
+
+
+
+# Python program to find the value of 'x' when f(x) = 0 using Binary Search on Continuous Space
+import math
+
+# Define a continuous function 'f'
+def f(x):
+    return x**3 - 5 * x**2 + 5
+
+# TODO: Write the Binary Search Function that performs the search on the continuous function in the interval [2, 5]
+
+
+def perform_binary_search(func, target, right, left, precision):
+    while right - left > precision:
+        middle = (right + left) / 2
+        if func(middle) > target:
+            right = middle
+        else:
+            left = middle
+    return (right + left) / 2
+# TODO: Define precision, target value, and interval bounds
+precision = 1e-6
+target = 0 
+interval = [2,5]
+left = interval[0]
+right = interval[1]
+
+print(perform_binary_search(f,target,right,left,precision))
